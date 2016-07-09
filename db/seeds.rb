b0b80f69ad5ee2400332cb4f6b9dc303c502f0fa
David Freerksen <dfreerksen@gmail.com>
2016-07-08 18:59:04 -0700

    Double quotes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,7 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
 # Archangel seed data
 Archangel::Engine.load_seed
-
