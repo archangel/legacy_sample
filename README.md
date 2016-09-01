f5ecbf41735017d70f916dfdc5e3b6d787538187
David Freerksen <dfreerksen@gmail.com>
2016-08-31 21:37:48 -0700

    Sendgrid

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Demo application for the Archangel.
 
 ```
 bundle install
+bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
 ```
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,7 @@
+CarrierWave.configure do |config|
+  # Local file storage
+  config.storage = :file
+
+  # Want to use upload files to AWS S3? Take a look at Carrierwave documentation
+  # https://github.com/carrierwaveuploader/carrierwave#fog
+end
